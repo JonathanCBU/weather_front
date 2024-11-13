4b361248a58ee9ec9c2ddd9c402ef367df6265fe
Jonathan Cook <jcookbme@gmail.com>
2024-11-13 11:43:37 -0500

    add icons and

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Weather.css";
+import day_img from "../assets/day.svg";
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="weather">
+      <div className="search">
+        <input type="text" placeholder="city or zip" />
+      </div>
+      <img src={day_img} alt="" className="weather-icon" />
+      <div className="temp">
+        <p className="farenheit">32</p>
+        <p className="celsius">0</p>
+      </div>
     </div>
   );
 };
