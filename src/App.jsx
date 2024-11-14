a575ae7b278a35e06a3ddf772466b5f4ae35f90c
Jonathan Cook <jcookbme@gmail.com>
2024-11-14 07:10:18 -0500

    implement search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import Today from "./components/Today";
-import Search from "./components/Search";
+import Weather from "./components/Weather";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Search = () => {
+  const inputRef = useRef();
+
+  const [currentData, setCurrentData] = useState(false);
+  const [forecastData, setForecastData] = useState(false);
+
+  const fetchApi = async (location) => {
+    try {
+      console.log(location);
+      const url = `http://127.0.0.1:8080/weather/${location}`;
+      const response = await axios.get(url);
+      console.log(response.data);
+      setCurrentData(response.data.current);
+      setForecastData(response.data.forcast);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi("Boston");
+  }, []);
+
+  return (
+    <div className="search">
+      <form onSubmit={() => fetchApi(inputRef.current.value)}>
+        <input type="text" placeholder="city or zip" ref={inputRef} />
+      </form>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div className="app">
-      <Search />
-      <Today />
+      <Weather />
     </div>
   );
 };
--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./style/Today.css";
-import day_img from "../assets/day.svg";
+import day_img from "../assets/clear.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Today = () => {
   const [weather, setWeather] = useState({});
 
-  const fetchApi = async () => {
-    const response = await axios.get("http://127.0.0.1:8080/api");
-    console.log(response.data.hello);
-  };
+  // const fetchApi = async () => {
+  //   // const response = await axios.get("/weather/Brooklyn");
 
-  useEffect(() => {
-    fetchApi();
-  }, []);
+  //   // TODO: figure out what is cached that's not updating my proxy
+  //   const response = await axios.get("http://127.0.0.1:8080/weather/Brooklyn");
+
+  //   console.log(response.data);
+  // };
+
+  // useEffect(() => {
+  //   fetchApi();
+  // }, []);
 
   return (
     <div className="today">
@@ -24,11 +28,13 @@ const Today = () => {
       </div>
       <div className="numericInfo">
         <div className="temp">
-          <p className="farenheit">32</p>
-          <p className="celsius">0</p>
+          <p className="farenheit">temp_f</p>
+          <p className="celsius">temp_c</p>
         </div>
-        <div className="humidity"></div>
-        <div className="wind"></div>
+        <div className="air">
+          <p>hume</p>
+          <p>wind</p>
+        </div>
       </div>
     </div>
   );
